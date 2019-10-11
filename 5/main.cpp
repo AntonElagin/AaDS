6e9cd64c8d50f5bdd35e6994d5449874ac36199e
AntonElagin <anton120299@gmail.com>
2019-10-11 20:34:23 +0300

    start project

---
+++ 5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class date{
+public:
+
+private:
+  std::string type;
+  int day;
+  int month;
+  int year;
+};
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
